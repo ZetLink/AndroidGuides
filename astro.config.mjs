ec15c98e17bfadac516d07864602a9bbf98dcde7
ZetLink <royalezetlink@gmail.com>
2025-01-06 17:40:52 -0300

    AndroidGuides: Adapt to use Markdoc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,40 +2,40 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import markdoc from '@astrojs/markdoc';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Android Guides',
-			lastUpdated: true,
-			social: {
-				github: 'https://github.com/withastro/starlight',
-				telegram: 'https://t.me/ZetLinkUwU',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Preparations', slug: 'starting/preparations' },
-						{ label: 'Download ROM Source', slug: 'starting/download-rom' },
-						{ label: 'Download DeviceTree', slug: 'starting/download-dt' },
-					],
-				},
-				{
-					label: 'Compilation Time',
-					items: [
-						{ label: 'Compile LineageOS', slug: 'compile/compile-rom' },
-						{ label: 'Compile Derpfest', slug: 'compile/compile-derp' },
-					],
-				},
-				{
-					label: 'Extra',
-					items: [
-						{ label: 'Install ADB', slug: 'extra/install-adb' },
-						{ label: 'Install Custom ROM', slug: 'extra/install-rom' },
-					],
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'Android Guides',
+        lastUpdated: true,
+        social: {
+            github: 'https://github.com/withastro/starlight',
+            telegram: 'https://t.me/ZetLinkUwU',
+        },
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    { label: 'Preparations', slug: 'starting/preparations' },
+                    { label: 'Download ROM Source', slug: 'starting/download-rom' },
+                    { label: 'Download DeviceTree', slug: 'starting/download-dt' },
+                ],
+            },
+            {
+                label: 'Compilation Time',
+                items: [
+                    { label: 'Compile LineageOS', slug: 'compile/compile-rom' },
+                    { label: 'Compile Derpfest', slug: 'compile/compile-derp' },
+                ],
+            },
+            {
+                label: 'Extra',
+                items: [
+                    { label: 'Install ADB', slug: 'extra/install-adb' },
+                    { label: 'Install Custom ROM', slug: 'extra/install-rom' },
+                ],
+            },
+        ],
+		}), markdoc()],
 });
